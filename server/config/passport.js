e7e79670360420de4b587fd0b411075a591e0fe1
Denny Scott <denny@appnovation.com>
2015-10-05 21:51:39 -0700

    Mid server

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,6 +6,16 @@ var _super = require('sails-permissions/config/passport');
 _.merge(exports, _super);
 _.merge(exports, {
 
+  atlassian: {
+    name: 'Atlassian',
+    protocol: 'oauth',
+    strategy: require('passport-atlassian-oauth').Strategy,
+    options: {
+      consumerKey: 'atlassian-oauth-sample',
+      consumerSecret: 'denny'
+    }
+  },
+
   // Extend with custom logic here by adding additional fields, methods, etc.
 
 });
